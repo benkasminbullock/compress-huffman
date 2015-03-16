1114376a5159d3582275ef0355ffb9d0e03230d0
Ben Bullock <benkasminbullock@gmail.com>
2015-03-16 17:07:50 +0900

    Add skeleton and simple test.

--- lib/Compress/Huffman.pm
+++ lib/Compress/Huffman.pm
@@ -23,5 +23,48 @@ require Exporter;
 use warnings;
 use strict;
 use Carp;
+use Scalar::Util 'looks_like_number';
 our $VERSION = 0.01;
+
+sub new
+{
+    return bless {};
+}
+
+sub symbols
+{
+    # Object and the table of symbols.
+    my ($o, $s, %options) = @_;
+    # Check $s is a hash reference.
+    if (ref $s ne 'HASH') {
+	croak "Use as \$o->symbols (\\\%symboltable, options...)";
+    }
+    # Check we have numbers.
+    for my $k (keys %$s) {
+	if (! looks_like_number ($s->{$k})) {
+	    croak "Non-numerical value '$s->{$k}' for key '$k'";
+	}
+    }
+    my $size = $options{size};
+    if (! defined $size) {
+	$size = 2;
+    }
+    if ($size < 2 || int ($size) != $size) {
+	croak "Bad size $size for Huffman table, must be integer >= 2";
+    }
+    # If this is supposed to be a probability distribution, check
+    my $notprob = $options{notprob};
+    if (! $notprob) {
+	my $total = 0.0;
+	for my $k (keys %$s) {
+	    $total += $s->{$k};
+	}
+	if (abs ($total - 1.0) > 0.0001) {
+	    croak "Input values don't sum to 1.0; use \$o->symbols (\\\%s, notprob => 1) if not a probability distribution";
+	}
+    }
+#    if ($size 
+#	my $dummies = 	
+}
+
 1;
--- t/compress-huffman.t
+++ t/compress-huffman.t
@@ -4,7 +4,17 @@ use warnings;
 use strict;
 use Test::More;
 use Compress::Huffman;
-
+my $n = Compress::Huffman->new ();
+ok ($n, "Made object");
+my %s = (
+    a => 1,
+    b => 2,
+    c => 3,
+);
+eval {
+    $n->symbols (\%s, notprob => 1, verbose => 1);
+};
+ok (! $@, "Made symbol table");
 done_testing ();
 # Local variables:
 # mode: perl
