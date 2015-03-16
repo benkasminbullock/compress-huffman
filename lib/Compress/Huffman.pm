f20e64e0e09cbe3f9bb634be4e7ca129229271b3
Ben Bullock <benkasminbullock@gmail.com>
2015-03-16 16:53:17 +0900

    Initial commit of compress-huffman

---
+++ lib/Compress/Huffman.pm
@@ -0,0 +1,27 @@
+=encoding UTF-8
+
+=head1 NAME
+
+Compress::Huffman - abstract here.
+
+=head1 SYNOPSIS
+
+    use Compress::Huffman;
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut
+package Compress::Huffman;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw//;
+%EXPORT_TAGS = (
+    all => \@EXPORT_OK,
+);
+use warnings;
+use strict;
+use Carp;
+our $VERSION = 0.01;
+1;
---
+++ t/compress-huffman.t
@@ -0,0 +1,11 @@
+# This is a test for module Compress::Huffman.
+
+use warnings;
+use strict;
+use Test::More;
+use Compress::Huffman;
+
+done_testing ();
+# Local variables:
+# mode: perl
+# End:
