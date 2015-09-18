98a9717eb260828ad978edcbb0d8fbe84ed37b28
Ben Bullock <benkasminbullock@gmail.com>
2015-09-18 15:22:20 +0900

    Add README and code/test additions.

--- lib/Compress/Huffman.pm
+++ lib/Compress/Huffman.pm
@@ -24,6 +24,7 @@ use warnings;
 use strict;
 use Carp;
 use Scalar::Util 'looks_like_number';
+use POSIX qw/ceil/;
 our $VERSION = 0.01;
 
 sub new
@@ -35,9 +36,23 @@ sub symbols
 {
     # Object and the table of symbols.
     my ($o, $s, %options) = @_;
+    # Debugging output switch.
+    my $verbose;
+    if ($options{verbose}) {
+	$verbose = 1;
+    }
     # Check $s is a hash reference.
     if (ref $s ne 'HASH') {
 	croak "Use as \$o->symbols (\\\%symboltable, options...)";
+    }
+    # Copy the symbol table into our own thing. We need to put extra
+    # symbols in to it.
+    my %c = %$s;
+    $o->{c} = \%c;
+    # The number of symbols we encode with this Huffman code.
+    my $nentries = scalar keys %$s;
+    if (! $nentries) {
+	croak "Symbol table has no entries";
     }
     # Check we have numbers.
     for my $k (keys %$s) {
@@ -45,12 +60,21 @@ sub symbols
 	    croak "Non-numerical value '$s->{$k}' for key '$k'";
 	}
     }
+    if ($verbose) {
+	print "Checked for numerical keys.\n";
+    }
     my $size = $options{size};
     if (! defined $size) {
 	$size = 2;
     }
     if ($size < 2 || int ($size) != $size) {
 	croak "Bad size $size for Huffman table, must be integer >= 2";
+    }
+    if ($size > 10 && ! $options{alphabet}) {
+	croak "Use \$o->symbols (\%t, alphabet => ['a', 'b',...]) for table sizes bigger than 10";
+    }
+    if ($verbose) {
+	print "Set size of Huffman code alphabet to $size.\n";
     }
     # If this is supposed to be a probability distribution, check
     my $notprob = $options{notprob};
@@ -62,9 +86,106 @@ sub symbols
 	if (abs ($total - 1.0) > 0.0001) {
 	    croak "Input values don't sum to 1.0; use \$o->symbols (\\\%s, notprob => 1) if not a probability distribution";
 	}
+	if ($verbose) {
+	    print "Is a valid probability distribution (total = $total).\n";
+	}
     }
-#    if ($size 
-#	my $dummies = 	
+    # The number of tables. We need $t - 1 pointers to tables, which
+    # each require one table entry, so $t is the smallest number which
+    # satisfies
+    #
+    # $t * $size >= $nentries + $t - 1
+
+    my $t = ceil (($nentries -1) / ($size - 1));
+    if ($verbose) {
+	print "This symbol table requires $t Huffman tables of size $size.\n";
+    }
+    my $ndummies = 0;
+    if ($size > 2) {
+	# The number of dummy entries we need is
+	my $ndummies = $t * ($size - 1) - $nentries + 1;
+	if ($verbose) {
+	    print "The Huffman tables need $ndummies dummy entries.\n";
+	}
+	if ($ndummies > 0) {
+	    # Insert $ndummies dummy entries with probability zero into
+	    # our copy of the symbol table.
+	    for (0..$ndummies - 1) {
+		my $dummy = "dummy$_";
+		if ($c{$dummy}) {
+		    croak "The symbol table already has an entry '$dummy'";
+		}
+		$c{$dummy} = 0.0;
+	    }
+	}
+    }
+    # The end-product, the Huffman encoding of the symbol table.
+    my %h;
+    my $nfake = 0;
+    my %fakes;
+    while ($nfake < $t) {
+	my @keys = sort {$c{$a} <=> $c{$b}} keys %c;
+	# The total weight of this table.
+	my $total = 0;
+	# The next table
+	my @huff;
+	for my $i (0..$size - 1) {
+	    my $k = $keys[$i];
+	    if (! defined $k) {
+		last;
+	    }
+#	    print "$i: $k $c{$k}\n";
+	    push @huff, $k;
+	    if ($k =~ /^fake/) {
+		addcodetosubtable (\%fakes, \%h, $k, $size, $i);
+	    }
+	    $total += $c{$k};
+	    delete $c{$k};
+	    $h{$k} = $i;
+	}
+	my $fakekey = 'fake'.$nfake;
+	$c{$fakekey} = $total;
+	$fakes{$fakekey} = \@huff;
+	$nfake++;
+    }
+    for my $k (keys %h) {
+	if ($k =~ /fake|dummy/) {
+	    delete $h{$k};
+	}
+    }
+    $o->{h} = \%h;
+    $o->{s} = $s;
+}
+
+sub xl
+{
+    my ($o) = @_;
+    my $h = $o->{h};
+    my $s = $o->{s};
+    die unless $h && $s;
+    my $len = 0.0;
+    for my $k (keys %$h) {
+	$len += length ($h->{$k}) * $s->{$k};
+	print "$k $h->{$k} $s->{$k} $len\n";
+    }
+    return $len;
+}
+
+# Add the prefix $i to everything underneath us.
+
+sub addcodetosubtable
+{
+    my ($fakes, $h, $k, $size, $i) = @_;
+    my $subhuff = $fakes->{$k};
+    for my $j (0..$size - 1) {
+	my $subk = $subhuff->[$j];
+	if ($subk =~ /^fake/) {
+	    addcodetosubtable ($fakes, $h, $subk, $size, $i);
+	}
+	else {
+	    $h->{$subk} = $i . $h->{$subk};
+	}
+    }
 }
 
 1;
--- t/compress-huffman.t
+++ t/compress-huffman.t
@@ -15,7 +15,22 @@ eval {
     $n->symbols (\%s, notprob => 1, verbose => 1);
 };
 ok (! $@, "Made symbol table");
+my %t = (
+    a => 0.01,
+    b => 0.02,
+    c => 0.03,
+    d => 0.04,
+    e => 0.2,
+    f => 0.3,
+    g => 0.4,
+);
+$n->symbols (\%t);
+$n->symbols (\%t, size => 3, verbose => 1);
+$n->symbols (\%t, size => 4, verbose => 1);
+#$n->symbols (\%t, size => 99, verbose => 1);
+
 done_testing ();
+
 # Local variables:
 # mode: perl
 # End:
